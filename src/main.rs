dfce10a55d706c90d3914372691bdd96782b399b
Benjamin Pinter <ben.david.pinter@outlook.com>
2016-02-27 01:17:41 -0500

    Classic concurrenty in Rust

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::{Mutex, Arc};
+
+
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+
+
+impl Philosopher {
+    fn new(name :&str, left: usize, right:usize) -> Philosopher  {
+        Philosopher{
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+        println!("{} is staring to eat!", &self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} is done eating!", &self.name);
+    }
+}
+
+fn main() {
+
+    let table = Arc::new(Table {forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        ]});
+
+    let philosophers = vec![
+        Philosopher::new("David Pooper", 0, 1),
+        Philosopher::new("Other Dude", 1, 2),
+        Philosopher::new("Ashley Mont",2 ,3),
+        Philosopher::new("Chris Fox",3,4),
+        Philosopher::new("Ben Pinter",0,4),
+        ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
